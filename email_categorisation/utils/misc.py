2fa947beec6f26caf2883f3b200b73452a9c1f1a
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-24 18:59:52 +0530

    Delete email_categorisation directory

--- email_categorisation/utils/misc.py
+++
@@ -1,8 +0,0 @@
-import tomlkit
-
-
-def get_project_meta(file):
-    with open(file) as pyproject:
-        file_contents = pyproject.read()
-
-    return tomlkit.parse(file_contents)["tool"]["poetry"]
