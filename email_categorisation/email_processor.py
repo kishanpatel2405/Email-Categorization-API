fb448cc815fb4b2c8d83503f26845640f14314dd
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-24 16:26:13 +0530

    Delete email_categorisation directory

--- email_categorisation/email_processor.py
+++
@@ -1,9 +0,0 @@
-# email_processor.py
-import asyncio
-from core.event_handlers import process_emails
-
-async def main():
-    await process_emails()
-
-if __name__ == "__main__":
-    asyncio.run(main())
